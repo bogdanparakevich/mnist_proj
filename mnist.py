a7bd2d3e2c668cbfe63577dc3a12ea02f57b7cc7
Bahdan Parakevich <bogdanparakevich@gmail.com>
2021-09-27 14:49:35 +0300

    all new

--- mnist.py
+++ mnist.py
@@ -1,23 +1,42 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from sklearn.utils import shuffle
 
 
 data = pd.read_csv('/Users/bogdan_parakevich/mnist_proj/train.csv')
-
 data = np.array(data)
-np.random.shuffle(data) # shuffle before splitting into dev and training sets
+
+epochs = 21
+alpha = 0.1
+momentum = 0.9
+change_W1 = 0.001
+change_b1 = 0.001
+change_W2 = 0.001
+change_b2 = 0.001
+
+np.random.shuffle(data)
 data = data.T
-
-Y_train = data[0, 1000:]
-Y_true = Y_train
-
-X_train = data[1:,1000:]
+n_test = 4200
+train_data = data[:, n_test:]
+m = train_data.shape[1]
+Y_train = train_data[0]
+X_train = train_data[1:]
 X_train = X_train / 255.
+Y_true_matrix = np.zeros((m, 10))
+Y_true_matrix[np.arange(m), Y_train] = 1
+Y_true_matrix = Y_true_matrix.T
+n_batchs = 32
+X_batches = np.array_split(X_train, n_batchs, axis=1)
+Y_batches = np.array_split(Y_true_matrix, n_batchs, axis=1)
 
-Y_dev = data[0,:1000]
-X_dev = data[1:,:1000]
-X_dev = X_dev / 255.
+test_data = data[:,:n_test]
+Y_test = data[0,:n_test]
+X_test = data[1:,:n_test]
+X_test = X_test / 255.
+Y_test_matrix = np.zeros((n_test, 10))
+Y_test_matrix[np.arange(n_test), Y_test] = 1
+Y_test_matrix = Y_test_matrix.T
 
 W1 = np.random.random((10, 784)) - 0.5
 b1 = np.random.random((10, 1)) - 0.5
@@ -37,64 +56,83 @@ def argmax(A):
 def ReLU_deriv(Z):
     return Z > 0
 
+def accuracy(pred, true):
+    return np.sum(pred == true) / true.size
 
-Z1 = (W1 @ X_train) + b1
-A1 = ReLU(Z1)
-Z2 = (W2 @ A1) + b2
-A2 = softmax(Z2)
-
-Y_true_matrix = np.zeros((41000, 10))
-Y_true_matrix[np.arange(41000), Y_true] = 1
-Y_true_matrix = Y_true_matrix.T
-
-epochs = 501
-alpha = 0.1
-m = A2.shape[1]
-
-def accuracy(Y_pred, Y_true_matrix):
-    return np.sum(Y_pred == Y_true_matrix) / Y_pred.size
-
-for i in range(epochs):
-    Z1 = (W1 @ X_train) + b1
-    A1 = ReLU(Z1)
-    Z2 = (W2 @ A1) + b2
-    A2 = softmax(Z2)
-
-    dloss_dZ2 = A2 - Y_true_matrix
-
-    dloss_dW2 = (1 / m) * dloss_dZ2 @ A1.T
-    dloss_db2 = (1 / m) * np.sum(dloss_dZ2)
-
-    dloss_dZ1 = W2.T @ dloss_dZ2 * ReLU_deriv(Z1)
-
-    dloss_dW1 = (1 / m) * dloss_dZ1 @ X_train.T
-    dloss_db1 = (1 / m) * np.sum(dloss_dZ1)
-
-    W1 = W1 - alpha * dloss_dW1
-    b1 = b1 - alpha * dloss_db1    
-    W2 = W2 - alpha * dloss_dW2  
-    b2 = b2 - alpha * dloss_db2
-    if i % 100 == 0:
-            print("Iteration: ", i)
-            Y_pred = argmax(A2)
-            print(accuracy(Y_pred, Y_true))
-
-
-def test_madel(W1,b1,W2,b2,X):
+def test_model(W1, b1, W2, b2, X):
     Z1 = (W1 @ X) + b1
     A1 = ReLU(Z1)
     Z2 = (W2 @ A1) + b2
     A2 = softmax(Z2)
-    A2_dev = A2
-    return A2_dev
+    return A2
 
-A2_dev = test_madel(W1,b1,W2,b2,X_dev)
-A2_dev = argmax(A2_dev)
+x_plot_train = []
+y_plot_train = []
+x_plot_test = []
+y_plot_test = []
+
+for i in range(epochs):
+    
+    X_batches, Y_batches = shuffle(X_batches, Y_batches, random_state=n_batchs)
+    
+    for k in range(n_batchs):
+        m_batch = Y_batches[k].shape[1]
+        Z1 = (W1 @ X_batches[k]) + b1
+        A1 = ReLU(Z1)
+        Z2 = (W2 @ A1) + b2
+        A2 = softmax(Z2)
+
+        dZ2 = A2 - Y_batches[k]
+        dW2 = 1 / m_batch * dZ2.dot(A1.T)
+        db2 = 1 / m_batch * np.sum(dZ2)
+        dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+        dW1 = 1 / m_batch * dZ1.dot(X_batches[k].T)
+        db1 = 1 / m_batch * np.sum(dZ1)
+
+        change_W1 = (momentum * change_W1) - (alpha * dW1)
+        change_b1 = (momentum * change_b1) - (alpha * db1)
+        change_W2 = (momentum * change_W2) - (alpha * dW2)
+        change_b2 = (momentum * change_b2) - (alpha * db2)
+
+        W1 = W1 + change_W1
+        b1 = b1 + change_b1
+        W2 = W2 + change_W2
+        b2 = b2 + change_b2
+        
+        e = 1e-6
+        loss = - (1 / m_batch) * np.sum(Y_batches[k] * np.log(A2))
+        
+        A2_test = test_model(W1, b1, W2, b2, X_test)
+        loss_test = - (1 / n_test) * np.sum(Y_test_matrix * np.log(A2_test))
+        
+        Y_pred = argmax(A2)
+        Y_true = argmax(Y_batches[k])
+        
+        Y_pred_test = argmax(A2_test)
+        Y_true_test = argmax(Y_test_matrix)
+        
+        k = k + 1
+        
+    if i % 5 == 0:
+        print('epochs:', i)
+        print('loss_train:', loss)
+        print('loss_test:', loss_test)
+        print('accuracy_train:', (np.sum(Y_pred == Y_true) / Y_true.size))
+        print('accuracy_test:', (np.sum(Y_pred_test == Y_true_test) / Y_true_test.size))
+        
+    x_plot_train.append(i)
+    y_plot_train.append(loss)
+    
+    x_plot_test.append(i)
+    y_plot_test.append(loss_test)
 
 
-accuracy_dev = accuracy(A2_dev, Y_dev)
-print('accuracy_dev =', accuracy_dev)
-
+plt.plot(x_plot_train, y_plot_train, "r", label="loss_train")
+plt.plot(x_plot_test, y_plot_test, "g", label="loss_test")
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.legend()
+plt.show() 
 
 
 
